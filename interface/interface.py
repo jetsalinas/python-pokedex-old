0262aeec3b739941eabadbb167f8743ea67e4d9b
jetsalinas <joseenrico.salinas@gmail.com>
2018-11-18 01:10:45 +0800

    Moved interface into own folder

--- interface/interface.py
+++ interface/interface.py
@@ -1,12 +1,16 @@
 import pyglet
 import glooey
 
+from .browser import Browser
+
 class Interface():
     
     def __init__(self, database):
         self.database = database
         self.window = pyglet.window.Window(width=1200, height=900)
         self.gui = glooey.Gui(self.window)
+
+        self.gui.add(Browser(self))
 
     def start(self):
         pyglet.app.run()
