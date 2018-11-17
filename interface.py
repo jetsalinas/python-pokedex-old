600b3bed3a82ded0059166c45efe985c181fffc2
jetsalinas <joseenrico.salinas@gmail.com>
2018-11-17 18:27:53 +0800

    Changed window size

--- interface.py
+++ interface.py
@@ -4,8 +4,9 @@ import glooey
 class Interface():
     
     def __init__(self, database):
-        window = pyglet.window.Window()
-        gui = glooey.Gui(window)
+        self.database = database
+        self.window = pyglet.window.Window(width=1200, height=900)
+        self.gui = glooey.Gui(self.window)
 
     def start(self):
         pyglet.app.run()
