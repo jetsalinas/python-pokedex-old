2508c921ab1ef67df8ba8ad6cdf90c8d7ddfcb85
jetsalinas <joseenrico.salinas@gmail.com>
2018-11-17 18:21:34 +0800

    Added database loading to interface

--- interface.py
+++ interface.py
@@ -3,7 +3,7 @@ import glooey
 
 class Interface():
     
-    def __init__(self):
+    def __init__(self, database):
         window = pyglet.window.Window()
         gui = glooey.Gui(window)
 
--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from files.database import Database
 if __name__ == "__main__":
 
     # TODO: handle command line arguments
-    # TODO: Implement csv loading
     database = Database()
-
-    interface = Interface()
+    interface = Interface(database)
     interface.start()
